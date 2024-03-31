3652bfa3869965e1bf2b51ff794f72efb3bf0acd
Oleg Skiba <oleg2720003@gmail.com>
2024-04-01 00:10:59 +0300

    Добавьте файлы проекта.

---
+++ WebApplication5/ItemModel.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApplication5
+{
+        public class ItemModel
+        {
+            public int ID { get; set; }
+            public string Name { get; set; }
+            public decimal Price { get; set; }
+        }
+}
---
+++ WebApplication5/Pages/Shared/_ItemsTable.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApplication5.Pages.Shared
+{
+    public class ItemsTableModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
